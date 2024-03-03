76932241b7da0a0d0152d099895a99b94ad817f7
Keith Greer <keith@kgreer.net>
2024-03-03 12:47:42 -0500

    Updated Bootstrap to 5.3.2 and added theme light/dark/system selector menu

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿///<reference path="../lib/jquery/jquery.js" />
 
-// Write your JavaScript code.
+function setTheme(theme) {
+    if (theme == 'light' || theme == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+    }
+};
+
+function saveTheme(theme) {
+
+    if (theme == 'light' || theme == 'dark') {
+        localStorage.setItem('Theme', theme);
+    }
+    else if (theme == 'system') {
+        localStorage.removeItem('Theme');
+    }
+
+    setTheme(getTheme());
+};
+
+window.addEventListener('storage', (event) => {
+    if (event.storageArea != localStorage) return;
+    if (event.key == 'Theme') {
+        setTheme(getTheme());
+    }
+});
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+    if (localStorage.getItem('Theme') == null) {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    }
+});
