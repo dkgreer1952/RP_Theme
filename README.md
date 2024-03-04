26f42231b82bd1dc65c9b04f3d54f7da7440daea
Keith Greer <keith@kgreer.net>
2024-03-04 07:22:09 -0500

    Updated documentation and added commenting.

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 ## Basic template Razor Pages application illustrating how to implement a light/dark/system selector menu for Bootstrap 5.3.0 and above.
 
 ### If Light or Dark is selected, that is stored in the browser's local storage (per machine) and overrides the Windows Light or Dark mode setting.
-### If System is selected, no setting is stored.  Instead, the application theme will default to the Windows mode setting.
-### If the browser is open on the application and the Windows mode is changed, the application will be notified and will change its theme accordingly.
+### If instead System is selected, no setting is stored and the theme will default to the Windows mode setting.
+### Notifications
+- If System was selected and the browser is open on the application and the Windows mode is changed, the application will be notified and will change its theme accordingly.
+- If multiple tabs are open on the application, changing the theme in one will change all open tabs.
+### Files of interest:
+- _Layout.cshtml
+- _LightDarkPartial.cshtml
+- site.js
 
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,13 @@
 ﻿///<reference path="../lib/jquery/jquery.js" />
 
+/* Set the Bootstrap theme attribute */
 function setTheme(theme) {
     if (theme == 'light' || theme == 'dark') {
         document.documentElement.setAttribute('data-bs-theme', theme);
     }
 };
 
+/* Persist the theme to browser storage, or remove it to default to the Windows mode */
 function saveTheme(theme) {
 
     if (theme == 'light' || theme == 'dark') {
@@ -18,6 +20,7 @@ function saveTheme(theme) {
     setTheme(getTheme());
 };
 
+/* If the theme was changed on any tab, set it on this tab */
 window.addEventListener('storage', (event) => {
     if (event.storageArea != localStorage) return;
     if (event.key == 'Theme') {
@@ -25,6 +28,7 @@ window.addEventListener('storage', (event) => {
     }
 });
 
+/* If the Windows mode has changed and no over-riding theme is in browser storage, set the Bootstrap mode to the Windows mode */
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
     if (localStorage.getItem('Theme') == null) {
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
